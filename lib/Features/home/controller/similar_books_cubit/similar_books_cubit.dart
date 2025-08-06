4b61ff9bc87616bb9ef3f5be18760b5dbab55db6
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-08-06 10:08:04 +0300

    create similar books cubit

--- lib/Features/home/controller/similar_books_cubit/similar_books_cubit.dart
+++ lib/Features/home/controller/similar_books_cubit/similar_books_cubit.dart
@@ -1,9 +1,29 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/Models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'similar_books_state.dart';
 
 class SimilarBooksCubit extends Cubit<SimilarBooksState> {
-  SimilarBooksCubit() : super(SimilarBooksInitial());
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fechSimilarBooks({required String category}) async {
+    emit(SimilarBooksLoading());
+
+    var result = await homeRepo.fetchSimilarBooks(category: category);
+
+    result.fold(
+      (failure) {
+        return left(
+          emit(SimilarBooksFailure(errorMessage: failure.errorMessage)),
+        );
+      },
+      (books) {
+        return right(emit(SimilarBooksSuccess(books: books)));
+      },
+    );
+  }
 }
