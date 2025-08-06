cb00256d8713043d157234cc9a6eb7f72a277cf6
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-08-06 10:43:03 +0300

    Add sliding text animation to SplashView

--- lib/Features/Splash/presentaion/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentaion/views/widgets/splash_view_body.dart
@@ -1,8 +1,45 @@
+import 'package:bookly_app/Features/Splash/presentaion/views/widgets/sliding_text.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +48,7 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.darkLogo),
-        Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 18),
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
