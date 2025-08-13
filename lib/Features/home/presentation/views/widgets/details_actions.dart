d3c9e92c36a9028324babd463d2305024d9b6a91
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-08-13 17:56:36 +0300

    Add URL launcher and toast functionality for book preview

--- lib/Features/home/presentation/views/widgets/details_actions.dart
+++ lib/Features/home/presentation/views/widgets/details_actions.dart
@@ -1,5 +1,11 @@
+import 'package:bookly_app/Features/home/controller/similar_books_cubit/similar_books_cubit.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/widgets/custom_button.dart';
+import 'package:bookly_app/core/utils/widgets/custom_toast_bar.dart';
+import 'package:delightful_toast/toast/utils/enums.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DetailsActions extends StatelessWidget {
   const DetailsActions({super.key});
@@ -29,7 +35,23 @@ class DetailsActions extends StatelessWidget {
               topRight: Radius.circular(16),
               bottomRight: Radius.circular(16),
             ),
-            onPressed: () {},
+            onPressed: () async {
+              var bookModel =
+                  BlocProvider.of<SimilarBooksCubit>(context).bookDetails;
+              Uri url = Uri.parse(bookModel.volumeInfo.previewLink!);
+              if (await canLaunchUrl(url)) {
+                await launchUrl(url);
+              } else {
+                customToastBar(
+                  context: context,
+                  message: 'Can\'t open this link $url',
+                  icon: Icons.error,
+                  backgroundColor: kPrimaryColor,
+                  textColor: Colors.white,
+                  postion: DelightSnackbarPosition.bottom,
+                );
+              }
+            },
           ),
         ),
       ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-import 'dart:developer';
-
 import 'package:bookly_app/Features/home/controller/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/Features/home/controller/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/models/user.dart';
 import 'package:bookly_app/core/simple_bloc_observer.dart';
-import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,9 +15,9 @@ void main() async {
   Bloc.observer = SimpleBlocObserver();
   runApp(const BooklyApp());
 
-  HomeRepoImpl homeRepoImpl = HomeRepoImpl(ApiService(Dio()));
-  User user = await homeRepoImpl.fetchUser();
-  log(user.toString());
+  // HomeRepoImpl homeRepoImpl = HomeRepoImpl(ApiService(Dio()));
+  // User user = await homeRepoImpl.fetchUser();
+  // log(user.toString());
 }
 
 class BooklyApp extends StatelessWidget {
